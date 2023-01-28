5afe76ce16c15f81645ffd0628d9ba279dd6311b
hamzakebbeh <100721033+hamzakebbeh@users.noreply.github.com>
2023-01-28 09:25:07 +0300

    division divisibility training interface (windows form)

---
+++ division divisibility training interface/final/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿using System;
+
+namespace final
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_sonuc = new System.Windows.Forms.Label();
+            this.txt_sayi = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lbl_kalan = new System.Windows.Forms.Label();
+            this.btn_hesapla = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_bol = new System.Windows.Forms.Button();
+            this.btn_kapat = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(304, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(435, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BÖLME BÖLÜNEBİLME KURALLARI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(352, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lütfen tam sayı giriniz(Bölünen):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(5, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(352, 58);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Girdiğiniz sayının bölünmesini\r\nistediğiniz sayıya seçiniz(Bölen)";
+            // 
+            // lbl_sonuc
+            // 
+            this.lbl_sonuc.AutoSize = true;
+            this.lbl_sonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_sonuc.Location = new System.Drawing.Point(846, 93);
+            this.lbl_sonuc.Name = "lbl_sonuc";
+            this.lbl_sonuc.Size = new System.Drawing.Size(19, 29);
+            this.lbl_sonuc.TabIndex = 4;
+            this.lbl_sonuc.Text = ".";
+            this.lbl_sonuc.Click += new System.EventHandler(this.lbl_sonuc_Click);
+            // 
+            // txt_sayi
+            // 
+            this.txt_sayi.Location = new System.Drawing.Point(364, 94);
+            this.txt_sayi.Name = "txt_sayi";
+            this.txt_sayi.Size = new System.Drawing.Size(109, 22);
+            this.txt_sayi.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(364, 189);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(109, 24);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // lbl_kalan
+            // 
+            this.lbl_kalan.AutoSize = true;
+            this.lbl_kalan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_kalan.Location = new System.Drawing.Point(848, 189);
+            this.lbl_kalan.Name = "lbl_kalan";
+            this.lbl_kalan.Size = new System.Drawing.Size(17, 25);
+            this.lbl_kalan.TabIndex = 7;
+            this.lbl_kalan.Text = ".";
+            this.lbl_kalan.Click += new System.EventHandler(this.lbl_kalan_Click);
+            // 
+            // btn_hesapla
+            // 
+            this.btn_hesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_hesapla.Location = new System.Drawing.Point(531, 116);
+            this.btn_hesapla.Name = "btn_hesapla";
+            this.btn_hesapla.Size = new System.Drawing.Size(139, 62);
+            this.btn_hesapla.TabIndex = 8;
+            this.btn_hesapla.Text = "=";
+            this.btn_hesapla.UseVisualStyleBackColor = true;
+            this.btn_hesapla.Click += new System.EventHandler(this.btn_hesapla_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(10, 277);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1356, 228);
+            this.listBox1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(746, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 22);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Bölüm =";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(751, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 22);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Kalan =";
+            // 
+            // btn_bol
+            // 
+            this.btn_bol.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_bol.Location = new System.Drawing.Point(380, 136);
+            this.btn_bol.Name = "btn_bol";
+            this.btn_bol.Size = new System.Drawing.Size(77, 29);
+            this.btn_bol.TabIndex = 12;
+            this.btn_bol.Text = "/";
+            this.btn_bol.UseVisualStyleBackColor = true;
+            this.btn_bol.Click += new System.EventHandler(this.btn_bol_Click);
+            // 
+            // btn_kapat
+            // 
+            this.btn_kapat.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_kapat.Location = new System.Drawing.Point(1179, 511);
+            this.btn_kapat.Name = "btn_kapat";
+            this.btn_kapat.Size = new System.Drawing.Size(187, 67);
+            this.btn_kapat.TabIndex = 13;
+            this.btn_kapat.Text = "kapat";
+            this.btn_kapat.UseVisualStyleBackColor = true;
+            this.btn_kapat.Click += new System.EventHandler(this.btn_kapat_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(957, 104);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(409, 84);
+            this.listBox2.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(5, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 25);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Kurallar:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1410, 620);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.btn_kapat);
+            this.Controls.Add(this.btn_bol);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_hesapla);
+            this.Controls.Add(this.lbl_kalan);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txt_sayi);
+            this.Controls.Add(this.lbl_sonuc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_sonuc;
+        private System.Windows.Forms.TextBox txt_sayi;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lbl_kalan;
+        private System.Windows.Forms.Button btn_hesapla;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_bol;
+        private System.Windows.Forms.Button btn_kapat;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ division divisibility training interface/final/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*string tipindeki verileri dizi içinde tanımlama*/
+        string[] kurallar = {"1 İle Bölünebilme:Her sayı bölünür.",
+        "2 İle Bölünebilme:Son rakamı çift sayı ise bölünür. Bir tam sayı 2 ile bölünmezse kalan her zaman 1 olur.",
+        "3 İle Bölünebilme:Rakamların değerleri toplamı 3 veya üçün katları ise bölünür.",
+        "4 İle Bölünebilme:Bir sayının birler ve onlar basamağı 00 ya da 4'ün katı ise sayı 4 ile bölünür.",
+        "5 İle Bölünebilme:Son rakamı 0 veya 5 ise 5'e bölünür.",
+        "6 İle Bölünebilme:Sayı hem 2'ye hem 3'e kalansız bölünebiliyorsa 6'ya da bölünür.",
+        "7 İle Bölünebilme:Sayının rakamlarının altına birler basamağından başlayarak sırasıyla; (+1),(+3),(+2),(-1),(-3),(-2),(+1)... sayılarıyla çarpılır. Elde edilen sayıların toplamı 7'nin tam katı ise bu sayı 7 ile tam bölünüyor demektir.",
+        "8 İle Bölünebilme:Son üç rakamı 8 ile tam bölünen ya da son üç rakamı 000 olan sayılar 8 e tam bölünebilir.",
+        "9 İle Bölünebilme:Rakamların sayı değerleri toplamı 9 veya dokuzun katlarıysa bölünür.",
+        "10 İle Bölünebilme:Son rakamı 0 ise bölünür."};
+        string[] sayılar = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <=10; i++)/*combobox için veri yazdırma */
+            {
+                comboBox1.Items.Add(i);
+            }
+        }
+
+        int sayii = 0;/* "sayii" int tipinde tanımlama ve 0 değeri atama*/
+        int A; /*A'yı tanımlama */
+        private void btn_hesapla_Click(object sender, EventArgs e)
+        {
+            int s1 = Convert.ToInt32(txt_sayi.Text);/* text box'ı int'e dönüştürmek ve s1 ataması*/
+            int s2 = Convert.ToInt32(comboBox1.Text);/* combo box'ı int'e dönüştürmek ve s2 ataması*/
+            lbl_sonuc.Text = (s1 / s2).ToString();/*label içinde aritmetik işlem (bölümü bulma)ve stringe çevirerek labele atamaktır */
+             lbl_kalan.Text = (s1 % s2).ToString();/*label içinde aritmetik işlem (kalanı bulma)ve stringe çevirerek labele atamaktır */
+            A = s1 % s2; /*int tipindeki A'ya kalanı atama*/
+            if (A > 0)/* kalan 0'dan büyük ise şartı*/
+            {
+                listBox2.Items.Add ("UYARI!!!!:" +s1+ " sayısı " +s2+ " saysına  tam bölünmemektedir"  );/*şart sağlanıyorsa ekrana yazdırlacak yazı*/
+            }
+
+            sayii = Convert.ToInt32(comboBox1.Text);
+
+            for (int j = 0; j < kurallar.Length ; j++)/*1.kural 10. kural arasında kalanlar  */
+            {
+                if (sayii.ToString() == sayılar[j])/*tür değişimi yapılıp şart ifadesi tanımlanmıştır*/
+                {
+                    listBox1.Items.Add(kurallar[j]);/*İllerin jsini eklenecektir*/
+                }
+            }
+        }
+
+        private void btn_bol_Click(object sender, EventArgs e)
+        {
+            int s1 = Convert.ToInt32(txt_sayi.Text);
+            int s2 = Convert.ToInt32(comboBox1.Text);
+            lbl_sonuc.Text = (s1 / s2).ToString();
+            lbl_kalan.Text = (s1 % s2).ToString();
+            A = s1 % s2;
+            if (A > 0)
+            {
+                listBox2.Items.Add( "UYARI!!!:" +s1 + " sayısı " + s2 + " saysına  tam bölünmemektedir");
+            }
+
+            sayii = Convert.ToInt32(comboBox1.Text);
+
+            for (int j = 0; j < kurallar.Length; j++)
+            {
+                if (sayii.ToString() == sayılar[j])
+                {
+                    listBox1.Items.Add(kurallar[j]);
+                }
+            }
+        }
+
+        private void btn_kapat_Click(object sender, EventArgs e)
+        {
+            DialogResult tus;
+            tus = MessageBox.Show("Kapatmak istediğinize emin misiniz? ", "HAMZA", MessageBoxButtons.YesNo, MessageBoxIcon.Question) ;/*kullanıcı uygulamayı kapatmak istediğinde karşısına açılacak olan evet ve hayır seçenekleri*/
+            if (tus == DialogResult.Yes)/*eğer eveti seçerse */
+            {
+                Close();/*uygulama kapanacaktır*/
+            }
+        }
+
+        private void lbl_sonuc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_kalan_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
